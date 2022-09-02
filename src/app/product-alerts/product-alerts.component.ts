8fef8abb8550e37db42a1994a06e944510b6daf5
mianuar <53685685+imananuar@users.noreply.github.com>
2022-09-02 15:27:46 +0800

    Routing

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -10,6 +10,8 @@ import { Product } from '../products';
 export class ProductAlertsComponent implements OnInit {
 
   @Input() product!: Product;
+  @Output() notify = new EventEmitter();
+  
 
   constructor() { }
 
